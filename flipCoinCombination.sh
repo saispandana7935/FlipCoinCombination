25d411d5e169cf1e89c854783c56005e2451a21a
saispandana7935 <saispandana7935@gmail.com>
2020-12-27 21:09:19 +0530

    Add. added a statement for winning combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,6 +3,68 @@
 read -p "enter number of times coins flipped " N
 isHead=1
 isTail=0
+headCount=0
+tailsCount=0
+declare -A Percentage
+declare -A CoinFlip
+for((i=1; i<=N; i++))
+do
+	coinFlip=$((RANDOM%2))
+	if [ $coinFlip -eq $isHead ]
+	then
+		CoinFlip[Heads]=$isHead
+		((headCount++))
+	else
+
+		CoinFlip[Tails]=$isTail
+		((tailsCount++))
+	fi
+done
+	Percentage["H"]=$(($headCount*100/$N))
+
+HHcount=0
+HTcount=0
+THcount=0
+TTcount=0
+declare -A Doublet
+for((i=1; i<=N; i++))
+do
+	coinFlip1=$((RANDOM%2))
+	coinFlip2=$((RANDOM%2))
+	if [ $coinFlip1 -eq $isHead  -a $coinFlip2 -eq $isHead ]
+	then
+		((HHcount++))
+		Doublet[HH]=$isHead
+		Doublet[HH]+=$isHead
+		HHpercent=$(($HHcount*100/$N))
+		Percentage["HH"]=$HHpercent
+	elif [ $coinFlip1 -eq $isHead  -a $coinFlip2 -eq $isTail ]
+	then
+		((HTcount++))
+		Doublet[HT]=$isHead
+		Doublet[HT]+=$isTail
+		HTpercent=$(($HTcount*100/$N))
+		Percentage["HT"]=$HTpercent
+
+	elif [ $coinFlip1 -eq $isTail  -a $coinFlip2 -eq $isHead ]
+	then
+		((THcount++))
+		Doublet[TH]=$isTail
+		Doublet[TH]+=$isHead
+		THpercent=$(($THcount*100/$N))
+		Percentage["TH"]=$THpercent
+
+	else
+		((TTcount++))
+		Doublet[TT]=$isTail
+		Doublet[TT]+=$isTail
+		TTpercent=$(($TTcount*100/$N))
+		Percentage["TT"]=$TTpercent
+
+	fi
+done
+
+echo ${Doublet[@]}
 HHHcount=0
 HHTcount=0
 HTHcount=0
@@ -12,7 +74,6 @@ HTTcount=0
 THTcount=0
 TTTcount=0
 declare -A Triplet
-declare -A Percentage
 for((i=1; i<=N; i++))
 do
 	coinFlip1=$((RANDOM%2))
@@ -87,6 +148,9 @@ do
 
 	fi
 done
+for key in ${!Percentage[@]}
+do
+	echo $key : ${Percentage[$key]}
+done
+winningCombination=$( printf '%s\n' ${Percentage[@]} | sort -n | tail -1 )
 
-echo ${Triplet[@]}
-echo ${Percentage[@]}
