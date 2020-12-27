eb47788a810acc52ed6fa461582ecd38f609677b
saispandana7935 <saispandana7935@gmail.com>
2020-12-27 18:22:43 +0530

    REFACTOR. calculate percentage of singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,22 @@
 #! /bin/bash -x
-coinFlip=$((RANDOM%2))
+
+read -p "enter number of times coins flipped " N
 isHead=1
 iaTail=0
-if [ $coinFlip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+headCount=0
+tailsCount=0
+declare -A CoinFlip
+for((i=1; i<=N; i++))
+do
+	coinFlip=$((RANDOM%2))
+	if [ $coinFlip -eq $isHead ]
+	then
+		CoinFlip[Heads]=$isHead
+		((headCount++))
+	else
+		
+		CoinFlip[Tails]=$isTail
+		((tailsCount++))
+	fi
+done
+	percent=$(($headCount*100/$N))
