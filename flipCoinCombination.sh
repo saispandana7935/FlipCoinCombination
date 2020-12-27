77557f4fc93b1c235de3c15a2f48809283df96a2
saispandana7935 <saispandana7935@gmail.com>
2020-12-27 19:49:47 +0530

    REFACTOR.. added statement to calculate Doublet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,21 +2,49 @@
 
 read -p "enter number of times coins flipped " N
 isHead=1
-iaTail=0
-headCount=0
-tailsCount=0
-declare -A CoinFlip
+isTail=0
+HHcount=0
+HTcount=0
+THcount=0
+TTcount=0
+declare -A Doublet
+declare -A Percentage
 for((i=1; i<=N; i++))
 do
-	coinFlip=$((RANDOM%2))
-	if [ $coinFlip -eq $isHead ]
+	coinFlip1=$((RANDOM%2))
+	coinFlip2=$((RANDOM%2))
+	if [ $coinFlip1 -eq $isHead  -a $coinFlip2 -eq $isHead ]
 	then
-		CoinFlip[Heads]=$isHead
-		((headCount++))
+		((HHcount++))
+		Doublet[HH]=$isHead
+		Doublet[HH]+=$isHead
+		HHpercent=$(($HHcount*100/$N))
+		Percentage["HH"]=$HHpercent
+	elif [ $coinFlip1 -eq $isHead  -a $coinFlip2 -eq $isTail ]
+	then
+		((HTcount++))
+		Doublet[HT]=$isHead
+		Doublet[HT]+=$isTail
+		HTpercent=$(($HTcount*100/$N))
+		Percentage["HT"]=$HTpercent
+
+	elif [ $coinFlip1 -eq $isTail  -a $coinFlip2 -eq $isHead ]
+	then
+		((THcount++))
+		Doublet[TH]=$isTail
+		Doublet[TH]+=$isHead
+		THpercent=$(($THcount*100/$N))
+		Percentage["TH"]=$THpercent
+
 	else
-		
-		CoinFlip[Tails]=$isTail
-		((tailsCount++))
+		((TTcount++))
+		Doublet[TT]=$isTail
+		Doublet[TT]+=$isTail
+		TTpercent=$(($TTcount*100/$N))
+		Percentage["TT"]=$TTpercent
+
 	fi
 done
-	percent=$(($headCount*100/$N))
+
+echo ${Doublet[@]}
+echo ${Percentage[@]}
